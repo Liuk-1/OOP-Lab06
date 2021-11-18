cc860b479d5574ee53866fde307b1d6836f8a589
Luca Lucioli <luca.lucioli@studio.unibo.it>
2021-11-19 00:50:28 +0100

    implemented graphimpl method

---
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -0,0 +1,43 @@
+package it.unibo.oop.lab06.generics1;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class GraphImpl<N> implements Graph<N> {
+
+	private Map<N, Set<N>> nodes = new HashMap<>();
+	
+	@Override
+	public void addNode(N node) {
+		if(node != null) {
+			this.nodes.putIfAbsent(node, null);
+		}
+	}
+
+	@Override
+	public void addEdge(N source, N target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Set<N> nodeSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Set<N> linkedNodes(N node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<N> getPath(N source, N target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
